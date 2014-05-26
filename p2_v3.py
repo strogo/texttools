df28736a555a7c0cc17061206f787788c2f5f09f
Your Name <prateek.gupta@nytimes.com>
2014-05-26 17:15:25 -0400

    Working version

--- p2_v3.py
+++ p2_v3.py
@@ -15,7 +15,7 @@ def main():
   Mdict = f[0]
   uniqueTags = f[1]
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 def getOutput(final):
   print "\n\t*Rule based entity search in progress...\n"
@@ -25,19 +25,21 @@ def getOutput(final):
 def extract():
   global Mdict
 
-
   if request.method== "POST":
     sentence= getSentence()
     processed_sentence = stripSentence(sentence)
     words_with_stopwords = processed_sentence.split() # gives list of words in sentence
     words = removeStopwords(words_with_stopwords)
 
-    final = []
-
+    final, index = [], 0
     while index < len(words): # when index = length of list, it will break
       l = checkinDictionary(words,index,sentence )
+      index = l[1]
 
-
+      if l[0] != None and l[0]!= []:
+        for a in l[0]:
+          if a not in final:
+            final.append(a)
 
     if len(final)==0:
       return "No Entity found.\n"
@@ -55,25 +57,29 @@ def checkinDictionary(words,index, sentence):
   global uniqueTags
   j = index
 
-
   if words[j] in Mdict:
     W = Mdict.getlist(words[j])
   else:
     return [None, j+1]
 
   entity = ""
+
   while len(set(W) & set(uniqueTags)) != 0:
     if words[j+1] in W:
       j, W = j+1, Mdict.getlist(words[j+1])
-      entity = entity + words[j] + " "
+    else:
+      break
+  # check for if W is not in the next words
 
-  tags = set(W).intersection(set(uniqueTags))
+  entity = " ".join(words[index:j+1])
+  tags = list(set(W).intersection(set(uniqueTags)))
 
   info = []
   for x,tag in enumerate(tags):
-    info[x] = {'Entity' : entity, 'Position': sentence.find(words[index]), 'Tag' : tag}
+#    print {'Entity' : entity, 'Position': sentence.find(words[index]) + 1, 'Tag' : tag}
+    info.append({'Entity' : entity, 'Position': sentence.find(words[index]) + 1, 'Tag' : tag})
 
-  return info
+  return [info, j+1]
 
 
   # PSEUDOCODE:
@@ -85,13 +91,8 @@ def checkinDictionary(words,index, sentence):
   # for x,i in enumerate(W):
   #     make info as  a list i.e. info = []
   # info[x] = {'Entity' : entity, 'Position': sentence.find(words[index]), 'Tag' = i}
-  # back trace the word: Sani --> Abacha Sani so need to backtrace it. Entity and tags are mixed in it
+### back trace the word: Sani --> Abacha Sani so need to backtrace it. Entity and tags are mixed in it
   # return [info, j]
-
-
-
-
-  return [index, info]
 
 def getSentence():
   converted_list= request.form.copy().keys()[0]
@@ -116,49 +117,9 @@ def removeStopwords(words_with_stopwords):
   for i in words_with_stopwords:
     if i not in stop:
       words.append(i)
+
   return words
 
-
-def getInfo(words, index, entity, tag, sentence, pattern):
-  info, count, max_intersect = {}, 0,0
-
-  for x,single_entity in enumerate(entity):
-      if re.search(pattern, single_entity) != None:
-        j = confirmEntity(words, index , single_entity, tags, x )
-        condition = j[0]
-        intersect = j[1]
-
-        if max_intersect <= intersect :
-          max_intersect = intersect
-          info[count] = {"intersect":intersect ,"Entity" : single_entity, "Position" : sentence.find(words[index])+1, "Tag" : tag[x]}
-          count = count + 1
-
-#  for i in info:
-#    if info[i]['intersect'] == max_intersect :
-#      info[count] = {"Entity" : info[i]['Entity'], "Position" : info[i]['Position'], "Tag" : info[i]["Tag"]}
-#      count = count + 1
-  print count
-  info = {"Word": words[index],"Entity" : info[count-1]['Entity'], "Position" : info[count-1]['Position'], "Tag" : info[count-1]['Tag']}
-
-  if count == 0:
-    return None
-  else:
-    return info
-
-def confirmEntity(words, index, single_entity, tags, x):
-  stripped_entity = stripSentence(single_entity)
-  entity_with_stopwords = stripped_entity.split() # gives list of words in sentence
-  entity_list = removeStopwords(entity_with_stopwords)
-
-  index = 1 if index == 0 else index
-
-  intersection = set([None if index else words[index-1],words[index],words[index+1]]).intersection(set(entity_list))
-
-  if len(intersection) >= 1:
-#    print index, str(set([words[index-1], words[index+1]])) + str(set(entity_list))
-    return [True, len(intersection) - 1]
-  else:
-    return [False, 0]
 
 if __name__ == "__main__":
   main()
