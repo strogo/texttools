31c23880d428d5c1e2a26be4c1379fa070e4161c
Your Name <prateek.gupta@nytimes.com>
2014-05-29 17:24:12 -0400

    Variant robust entity extractor. Yet to be completed

---
+++ super_dictionary.py
@@ -0,0 +1,9 @@
+######only need to be done once
+#read the text files line by line
+#remove everything after pipe
+#Get the canonical and form an entry in the dictionary
+# key of that component:decompose variant into its components
+#Everytime you get that canonical take the union of components of the variant
+#super dictionry formed : dumpt it in string form
+
+# use it as it is
